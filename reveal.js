d38e2fdc68faa8af4cd06bef8151bae2a27bba02
Leonardo Simões da Silva <Leonardo_leo_9@hotmail.com>
2024-04-14 22:31:51 -0300

    Final CSS widht desktop e add ScrollReveal.js
    Falta os media screens

---
+++ reveal.js
@@ -0,0 +1,36 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.seccoes',{
+    duration:2000,
+    distance:'90px'
+})
+
+revelar.reveal('#conteudo-main',{
+    duration:2000,
+    distance:'90px'
+})
+
+revelar.reveal('.hiden1',{
+    duration:1000,
+    distance:'90px',
+    delay: 500
+})
+
+revelar.reveal('.hiden2',{
+    duration:1000,
+    distance:'90px',
+    delay: 750
+})
+
+revelar.reveal('.hiden3',{
+    duration:1000,
+    distance:'90px',
+    delay: 900
+})
+
+revelar.reveal('.hiden4',{
+    duration:1000,
+    distance:'90px',
+    delay: 1250
+})
